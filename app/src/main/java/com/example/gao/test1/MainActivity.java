0aaa8415be951c881032a97e792f59524541c7d9
isoft168 <408230970@qq.com>
2018-07-17 15:43:12 +0800

    First Commit

---
+++ app/src/main/java/com/example/gao/test1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.gao.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.ObjectInputStream;
+
+public class MainActivity extends AppCompatActivity {
+    //Button testButton=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout2);
+
+        //testButton=
+
+
+
+    }
+}
